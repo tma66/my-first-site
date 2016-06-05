f5b1693c0fe98720e9cad0aea96bdcf61ae535fd
Tuling Ma <tuling.ma@gmail.com>
2016-06-04 23:18:07 -0700

    Allow heroku to find static files

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -122,6 +122,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
